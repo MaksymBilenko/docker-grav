958da7d2a9ec866a1fed226507c9275b21aa850c
Maksym Bilenko <Maksym_Bilenko@epam.com>
2017-07-18 13:51:08 +0300

    Reorganize initial checkout process

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,21 @@ case $1 in
 		if [ -z "$(ls -A /var/www/grav)" ]; then
 			echo "Checking out initial grav"
 			cd /var/www
-			git clone -b ${BRANCH} https://github.com/getgrav/grav.git
+
+			git clone https://github.com/getgrav/grav.git
+			git fetch --all --tags
+			git checkout ${BRANCH}
+
 			cd /var/www/grav
+
 			bin/grav install
+
 			echo "Installing admin plugin"
 			bin/gpm install admin -y
 		else
 			echo "Updating and starting grav"
 			cd /var/www/grav
+			
 			git fetch --all --tags
 			git checkout ${BRANCH}
 		fi
